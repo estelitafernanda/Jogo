9f63eccc22a8632e28d8b101f60c86b19a782052
Estelita Fernanda <112116442+estelitafernanda@users.noreply.github.com>
2023-11-18 19:44:49 -0300

    Add files via upload
    Códigos e imagens do desenvolvimento do jogo

---
+++ Jogo/js/jogo.js
@@ -0,0 +1,34 @@
+let canvas = document.querySelector('canvas');
+let context = canvas.getContext('2d');
+
+const desiredFPS = 120; 
+const frameTime = 1000 / desiredFPS; 
+
+let prevTime =  performance.now();
+let lag = 0;
+
+function animate(){
+    const currentTime = performance.now();
+    const elapsed = currentTime - prevTime;
+    prevTime = currentTime;
+    lag += elapsed;
+
+    
+    handleControls();
+    
+    while (lag >= frameTime) {
+       
+        background.update();
+        bonequinha.update(); 
+
+        lag -= frameTime;
+
+    }
+
+    window.requestAnimationFrame(animate)  
+   
+} 
+animate();
+
+  
+
